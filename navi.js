ffed7b0856af59143c207cf0733c5cc61777c672
Rafa-roni24 <rafadhila.shereen46@sd.belajar.id>
2025-11-23 16:11:12 +0700

    Add files via upload

--- navi.js
+++ navi.js
@@ -1,3 +1,4 @@
 function doitnow(){
     document.getElementById("someText").innerText = "some other important text"
+    document.getElementById("slideshow").src= "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png";
 }
