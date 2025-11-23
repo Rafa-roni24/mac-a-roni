8fe951d687f71cdac02c462024b7354b4e88f6d8
Rafa-roni24 <rafadhila.shereen46@sd.belajar.id>
2025-11-23 16:38:25 +0700

    Add files via upload

--- navi.js
+++ navi.js
@@ -1,4 +1,14 @@
+var counter = 0;
+
+var theList = {
+    "main" : [
+    "https://media.tenor.co/j2nm-x4S08MAAAAM/african-kid-africa.gif", "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png"
+    ]
+} 
+
 function doitnow(){
-    document.getElementById("someText").innerText = "some other important text"
-    document.getElementById("slideshow").src= "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png";
+    //document.getElementById("someText").innerText = "some other important text"
+    //document.getElementById("slideshow").src= "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png";
+    counter++;
+    document.getElementById("slideshow").src = theList.main[counter];
 }
