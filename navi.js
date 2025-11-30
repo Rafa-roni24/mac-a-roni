28afa5a390fe6d853ae222658d57790d4fcb24fe
Rafa-roni24 <rafadhila.shereen46@sd.belajar.id>
2025-11-30 16:17:55 +0700

    Add files via upload

--- navi.js
+++ navi.js
@@ -1,10 +1,12 @@
 var counter = 0;
 
+/*
 var theList = {
     "main" : [
     "https://media.tenor.co/j2nm-x4S08MAAAAM/african-kid-africa.gif", "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png", "https://media.tenor.co/OOGWddTK-KMAAAAC/thunder-elmo.gif", "https://raw.githubusercontent.com/Rafa-roni24/mac-a-roni/49827b359a620d671e0748148f5b6e3d8fa6dc0e/assets/tux.png"
     ]
-} 
+}
+*/
 
 function doitnow(){
     //document.getElementById("someText").innerText = "some other important text"
