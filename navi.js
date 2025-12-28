41be157ac803ef10d1f5e00ceb7173bb89485a49
Rafa-roni24 <rafadhila.shereen46@sd.belajar.id>
2025-12-28 16:47:38 +0700

    Add files via upload

--- navi.js
+++ navi.js
@@ -1,21 +1,42 @@
 var counter = 0;
 var myObj;
-/*
-var theList = {
-    "main" : [
-    "https://media.tenor.co/j2nm-x4S08MAAAAM/african-kid-africa.gif", "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png", "https://media.tenor.co/OOGWddTK-KMAAAAC/thunder-elmo.gif", "https://raw.githubusercontent.com/Rafa-roni24/mac-a-roni/49827b359a620d671e0748148f5b6e3d8fa6dc0e/assets/tux.png"
-    ]
-}
-*/
+var img;
+var newSrc;
 
 function getList(){
+    img = document.getElementById("slideshow");
     const xhr = new XMLHttpRequest();
         xhr.open('GET', 'database.json', true);
 
         xhr.onload = function() {
             if (xhr.status === 200) {
                 myObj = JSON.parse(this.responseText);
+                newSrc = myObj.main[counter].url;
+                const overlayOpts = {
+                    spinnerIcon: 'ball-circus',
+                    overlayBackgroundColor: 'rgba(255,255,255,0.8)'
+                };
+                JsLoadingOverlay.show(overlayOpts);
+                img.onload = () => {
+                  JsLoadingOverlay.hide(); 
+                };
+                img.src = newSrc;
+                /*document.getElementById("author").innerText = myObj.main[counter].author;
                 document.getElementById("slideshow").src = myObj.main[counter].url;
+                */
+                let isClicked = false; // Initialize a flag
+
+                  document.getElementById("nb").addEventListener("click", () => {
+                    if (isClicked) return;
+
+                    JsLoadingOverlay.show(overlayOpts);
+                    
+                    img.onload = () => {
+                      JsLoadingOverlay.hide(); 
+                    };
+                    img.src = newSrc;
+                  });
+                
                 document.getElementById("author").innerText = myObj.main[counter].author;
             } else {
                 document.getElementById("output").innerHTML = 'Error fetching JSON:' + xhr.statusText;
@@ -26,19 +47,23 @@ function getList(){
 }
 
 function nextButton(){
-    console.log(counter)
-    if (counter == myObj.main.length-1) return;
+    // if (counter == myObj.main.length-1) return;
+    counter++;
+    img.src = myObj.main[counter].url;
+    console.log(counter, img.src, myObj.main[counter].author);
+    img.onload = () => {
+      JsLoadingOverlay.hide(); 
+    };
     //document.getElementById("someText").innerText = "some other important text"
     //document.getElementById("slideshow").src= "https://w7.pngwing.com/pngs/1008/247/png-transparent-multimedia-projectors-projector-thumbnail.png";
     document.getElementById("slideshow").src = myObj.main[counter].url;
     document.getElementById("author").innerText = myObj.main[counter].author;
-    counter++;
 }
 
 function prevButton(){
+    // if (counter == 0) return;
+    counter--;
     console.log(counter)
-    if (counter == 0) return;
     document.getElementById("slideshow").src = myObj.main[counter].url;
     document.getElementById("author").innerText = myObj.main[counter].author;
-    counter--;
 }
